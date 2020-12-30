43cdae8e1f153b3ce9b692e19cc83ce0e3bab06e
TheXerr0r <57012627+TheXerr0r@users.noreply.github.com>
2020-12-30 20:09:50 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 discriptin = "SubFucker is simple tool designed to help bughunters with enumerating subdomain status codes"
 setuptools.setup(
     name="SubFucker",
-    version="1.0.1",
+    version="1.0.2",
     author="Scorpion Shiled",
     author_email="support@scorpion-shield.com",
     auther_website ="https://www.scorpion-shield.com/",
